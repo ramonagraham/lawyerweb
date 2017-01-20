96ecab39ec0c3e3816c5452ef852bb3e06a25498
cbarbour86 <cbarbour@mail.greenriver.edu>
2017-01-20 12:41:48 -0800

    initial upload of header/footer

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<nav id="nav-main" class="nav-main" role="navigation">
+    <ul>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../bios.php">Attorneys</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</nav>
