9e0bc4ac2f73066c9774b337cb4d0f4bdb2ecd57
HillaryW <hillmariewag@gmail.com>
2017-01-20 12:45:46 -0800

    updated home page - more information

--- index.php
+++ index.php
@@ -9,9 +9,49 @@
 ?>
 
 
-<body>
+<body id="home">
+<?php
+include ("");
+?>
 
-<h1
+<h1>The Law Offices of Richard Levenson </h1>
+
+<h3>Full-service Law Firm focused on Real Estate, Business Organization, and Commercial Transaction</h3>
+
+<div>
+    <p>
+        The Law Office of Richard Levenson is a full-service law firm focused on real estate,
+        business organization and commercial transactional law. We have nearly 40 years of experience in commercial
+        real estate transactions and real property issues. Our practice also excels at advising businesses during each
+        stage of the business's life cycle, including formation, contract disputes, mergers and acquisitions, commercial
+        transactions, security issuances (angel and venture capital investment, private placement offerings and
+        small-scale equity crowdfunding), industry specific regulatory compliance and business dissolution.
+    </p>
+
+    <p>
+        We also have experience advising on issues ranging from international trade and sharia-compliant transactions
+        to tax issues and estate planning.
+    </p>
+</div>
+
+<div>
+
+    <p>
+        Local: 253-572-4109
+    </p>
+</div>
+
+<div>
+    <p>
+        Location:
+    <br>
+        621 Pacific Ave
+    <br>
+        Suite 209
+    <br>
+        Tacoma, WA 98402
+    </p>
+</div>
 
 </body>
 
