187f050eddce680fc6abf7be042b02809d4960c3
ramonagraham <rgraham8@mail.greenriver.edu>
2017-01-23 11:54:18 -0800

    picture placeholder test

--- bios.php
+++ bios.php
@@ -7,41 +7,59 @@
 ?>
 
 <php
+    include 'includes/mainpage.css';
     include '/includes/header.php';
 ?>
 
 <body>
+    <div>
+        <div>
+            <textarea rows="4" cols="20" placeholder="Place Picture Here"></textarea>
+        </div>
+        <div>
+            <h2 class="bios-heading">Richard Levenson</h2>
+        </div>
+        <div>
+            <p class="bios-text">Richard Levenson began practicing law in 1978.  He has over 30 years of experience advising
+                clients on complex commercial real estate transactions, business formation and business disputes.
+                His practice includes estate planning and providing legal advice on state tax law,
+                as well as general and complex civil litigation.</p>
+        </div>
+        <div>
+            <textarea rows="4" cols="20" placeholder="Place Picture Here"></textarea>
+        </div>
+        <div>
+            <h2 class="bios-heading">Arthur DeLong</h2>
+        </div>
+        <div>
+            <p class="bios-text">Arthur DeLong practices business and commercial transactional law, and advises clients
+                on matters relating to business formation, capital formation (equity and debt offerings,
+                including venture capital deals, private placement offerings, crowdfunding and small-equity offerings),
+                structured financial transactions and estate planning.  His practice also spans commercial real estate,
+                international trade and intellectual property law.   Arthur is a published author in the field
+                of Islamic Finance, and also advises on Sharia-compliant structured transactions and Islamic wills.</p>
+        </div>
+        <div>
+            <p class="bios-text">In addition to his law degree, Arthur holds a Master's Degree in International Business
+                from Albers School of Business, and a BA in Diplomacy/World Affairs and Spanish from Occidental College.
+                Arthur speaks fluent Spanish and enjoys studying Arabic in his free time.  Although not his primary
+                area of practice, Arthur also does significant pro bono work on international human rights issues
+                with the Iraqi Refugee Assistance Project.</p>
+        </div>
 
-    <textarea rows="4" cols="20" placeholder="Place Picture Here"></textarea>
-    <h2 class="bios-heading">Richard Levenson</h2>
+        <div>
+            <textarea rows="4" cols="20" placeholder="Place Picture Here"></textarea>
+        </div>
+        <div>
+            <h2 class="bios-heading">Bridget Howe</h2>
+        </div>
 
-    <p class="bios-text">Richard Levenson began practicing law in 1978.  He has over 30 years of experience advising
-        clients on complex commercial real estate transactions, business formation and business disputes.
-        His practice includes estate planning and providing legal advice on state tax law,
-        as well as general and complex civil litigation.</p>
-
-
-    <h2 class="bios-heading">Arthur DeLong</h2>
-
-    <p class="bios-text">Arthur DeLong practices business and commercial transactional law, and advises clients
-        on matters relating to business formation, capital formation (equity and debt offerings,
-        including venture capital deals, private placement offerings, crowdfunding and small-equity offerings),
-        structured financial transactions and estate planning.  His practice also spans commercial real estate,
-        international trade and intellectual property law.   Arthur is a published author in the field
-        of Islamic Finance, and also advises on Sharia-compliant structured transactions and Islamic wills.</p>
-
-    <p class="bios-text">In addition to his law degree, Arthur holds a Master's Degree in International Business
-        from Albers School of Business, and a BA in Diplomacy/World Affairs and Spanish from Occidental College.
-        Arthur speaks fluent Spanish and enjoys studying Arabic in his free time.  Although not his primary
-        area of practice, Arthur also does significant pro bono work on international human rights issues
-        with the Iraqi Refugee Assistance Project.</p>
-
-    <h2 class="bios-heading">Bridget Howe</h2>
-
-    <p class="bios-text">Bridget graduated from Western Washington University in 2001 with a major in Art History
-        and a minor in East Asian Studies. She earned her paralegal certificate from the
-        University Washington-Tacoma in 2006, and became a licensed Limited Practice Officer in 2012. </p>
-
+        <div>
+            <p class="bios-text">Bridget graduated from Western Washington University in 2001 with a major in Art History
+                and a minor in East Asian Studies. She earned her paralegal certificate from the
+                University Washington-Tacoma in 2006, and became a licensed Limited Practice Officer in 2012. </p>
+        </div>
+    </div>
 </body>
 
 <php
