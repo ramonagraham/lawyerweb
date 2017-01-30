8b7cd1be1fae2225e2d20cdf293d61e280afcc2b
ramonagraham <rgraham8@mail.greenriver.edu>
2017-01-30 11:44:41 -0800

    initial tiled template partially done

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE HTML>
+<!--
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>The Law Offices of Richard Levenson</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header -->
+    <header id="header" class="alt">
+
+        <nav>
+            <a href="#menu">Menu</a>
+        </nav>
+    </header>
+
+    <!-- Menu -->
+    <nav id="menu">
+        <ul class="links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="landing.php">Landing</a></li>
+            <li><a href="generic.php">Generic</a></li>
+            <li><a href="elements.php">Elements</a></li>
+        </ul>
+        <ul class="actions vertical">
+            <li><a href="#" class="button special fit">Get Started</a></li>
+            <li><a href="#" class="button fit">Log In</a></li>
+        </ul>
+    </nav>
+
+    <!-- Banner -->
+    <section id="banner" class="major">
+        <div class="inner">
+            <header class="major">
+                <h1>The Law Offices of Richard Levenson</h1>
+            </header>
+            <div class="content">
+                <p>Full-service law firm focused on real estate,
+                    business organization, and commercial transactions.</p>
+                <ul class="actions">
+                    <li><a href="#one" class="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <!-- Main -->
+    <div id="main">
+
+        <!-- One -->
+        <section id="one" class="tiles">
+            <article>
+									<span class="image">
+										<img src="images/pic01.jpg" alt="" />
+									</span>
+                <header class="major">
+                    <h3><a href="landing.php" class="link">The Firm</a></h3>
+
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic02.jpg" alt="" />
+									</span>
+                <header class="major">
+                    <h3><a href="landing.php" class="link">Real Estate</a></h3>
+
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic03.jpg" alt="" />
+									</span>
+                <header class="major">
+                    <h3><a href="landing.php" class="link">Business Organization</a></h3>
+
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic04.jpg" alt="" />
+									</span>
+                <header class="major">
+                    <h3><a href="landing.php" class="link">Estate Planning</a></h3>
+
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic05.jpg" alt="" />
+									</span>
+                <header class="major">
+                    <h3><a href="landing.php" class="link">Commercial Transactions</a></h3>
+
+                </header>
+            </article>
+            <article>
+									<span class="image">
+										<img src="images/pic06.jpg" alt="" />
+									</span>
+                <header class="major">
+                    <h3><a href="landing.php" class="link">Blog</a></h3>
+
+                </header>
+            </article>
+        </section>
+
+        <!-- Two -->
+        <section id="two">
+            <div class="inner">
+                <header class="major">
+                    <h2>Massa libero</h2>
+                </header>
+                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                <ul class="actions">
+                    <li><a href="landing.php" class="button next">Get Started</a></li>
+                </ul>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- Contact -->
+    <section id="contact">
+        <div class="inner">
+            <section>
+                <form method="post" action="#">
+                    <div class="field half first">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div class="field half">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div class="field">
+                        <label for="message">Message</label>
+                        <textarea name="message" id="message" rows="6"></textarea>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Send Message" class="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
+            </section>
+            <section class="split">
+                <section>
+                    <div class="contact-method">
+                        <span class="icon alt fa-envelope"></span>
+                        <h3>Email</h3>
+                        <a href="#">arthur@richardlevenson.com</a>
+                    </div>
+                </section>
+                <section>
+                    <div class="contact-method">
+                        <span class="icon alt fa-phone"></span>
+                        <h3>Phone</h3>
+                        <span>(253) 572-4109</span>
+                    </div>
+                </section>
+                <section>
+                    <div class="contact-method">
+                        <span class="icon alt fa-home"></span>
+                        <h3>Address</h3>
+                        <span>621 Pacific Ave<br />
+										Suite 209<br />
+										Tacoma, WA 98402</span>
+                    </div>
+                </section>
+            </section>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <div class="inner">
+            <ul class="icons">
+                <li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+                <!-- <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li> -->
+                <li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+                <li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+            </ul>
+            <ul class="copyright">
+                <li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+            </ul>
+        </div>
+    </footer>
+
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
