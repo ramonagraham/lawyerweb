6308cdc65c504add60b7c349039e35e81d5e4c19
cbarbour86 <cbarbour@mail.greenriver.edu>
2017-02-13 19:23:08 -0800

    added bootstrap classes to nav bar to make nav bar resize on smaller screen

--- header.php
+++ header.php
@@ -46,16 +46,31 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     <link rel="stylesheet" href="assets/css/navbar.css"/>
 
 </head>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Collapsed Nav Bar Div -->
+        <div class="center-span">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a id="nav-home" class="button special active" href="index.php">Home</a></li>
+                <li><a id="nav-services" class="button special" href="services.php">Services</a></li>
+                <li><a id="nav-bios" class="button special" href="bios.php">Our Team</a></li>
+                <li><a id="nav-contact" class="button special" href="contact.php">Contact Us</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
 <body>
-<div class="center-span">
-    <nav class="navbar-fixed-top">
-        <a id="nav-home" class="button special active" href="index.php">Home</a>
-        <a id="nav-services" class="button special" href="services.php">Services</a>
-        <a id="nav-bios" class="button special" href="bios.php">Our Team</a>
-        <a id="nav-contact" class="button special" href="contact.php">Contact Us</a>
-    </nav>
-</div>
-
 <!-- Wrapper -->
 <div id="wrapper">
     <!-- Menu -->
